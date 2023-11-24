159f55ad40518dcd1f2cc11ad3211aa1468489b9
OziomaEunice <onwubikooziomaeunice@gmail.com>
2023-11-24 14:47:49 +0000

    Device agent properties created and Program file setup

--- DeviceAgent.cs
+++ DeviceAgent.cs
@@ -11,5 +11,49 @@ namespace MAS
 {
     internal class DeviceAgent : Agent
     {
+        //create the device agent's properties: task and valuation
+        private int task;
+        private int valuation;
+
+        //create the device agent's constructor
+        public DeviceAgent(int task, int valuation)
+        {
+            this.task = task;
+            this.valuation = valuation;
+        }
+
+        //create the device agent's default constructor
+        public DeviceAgent() {}
+
+
+        //create the device agent's setup method
+        public override void Setup()
+        {
+            //create the device agent's behaviour
+            Send("auctioneer", $"search{valuation}"); //send the valuation to the auctioneer
+        }
+
+
+        //create the device agent's act method
+        public override void Act(Message messages)
+        {
+            //create the device agent's behaviour
+            if (messages.Content.ToString().Contains("bid")) //if the message contains the word "bid"
+            {
+                string[] message = messages.Content.ToString().Split(' '); //split the message into an array of strings
+                int bid = int.Parse(message[1]); //parse the second string in the array into an integer
+                if (bid > valuation) //if the bid is greater than the valuation
+                {
+                    Send("auctioneer", $"accept{bid}"); //send the bid to the auctioneer
+                }
+            }
+        }
+
+        //create the device agent's act dafult method
+        public override void ActDefault()
+        {
+            // default behaviour of the device agent
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ActressMas;
+using System;
+
+// this class belongs to the environment. It is responsible for creating the 
+// agents and the environment.
+
+namespace MAS
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // create a new environment
+            var env = new EnvironmentMas(100);
+
+            // create the auctioneer agent
+
+
+            // create the edge server agents
+
+
+            // create the device agents and add them to the environment
+            var device = new DeviceAgent(); env.Add(device, "device");
+
+
+            DeviceAgent device1 = new DeviceAgent(1, 20);
+            DeviceAgent device2 = new DeviceAgent(2, 20);
+
+            
+
+
+            // start the environment
+            env.Start();
+
+            Console.ReadLine(); //wait for the user to press enter
+        }
+    }
+}
